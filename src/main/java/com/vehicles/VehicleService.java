41b9a3ad6f3d05287a6c7a2653099751f085925e
Seetha <seethalakshmi1607@gmail.com>
2022-08-30 23:24:01 +0530

    printing the speed of the bike.

--- src/main/java/com/vehicles/VehicleService.java
+++ src/main/java/com/vehicles/VehicleService.java
@@ -10,6 +10,9 @@ package com.vehicles;
 public class VehicleService {
     public static void main(String[] args) {
         Bike myBike = new Bike("Honda", "CBR", "SportsBike");
+        // Printing the speed of the bike.
+        System.out.println("Bike type is SportsBike its speed is " + myBike.maxSpeed("sportsBike") + "km/h");
+
 
     }
 }
