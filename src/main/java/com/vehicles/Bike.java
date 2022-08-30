764353634924e468b0097ec9bcb999d285c5f0fe
Seetha <seethalakshmi1607@gmail.com>
2022-08-30 23:19:26 +0530

    created parametrized constructor of bike class

--- src/main/java/com/vehicles/Bike.java
+++ src/main/java/com/vehicles/Bike.java
@@ -3,6 +3,10 @@ package com.vehicles;
 public class Bike extends VehicleManufacturer implements Vehicle {
     private int maxSpeed;
 
+    public Bike(String vehicleName, String vehicleModelName, String vehicleType) {
+        super(vehicleName, vehicleModelName, vehicleType);
+    }
+
     @Override
     public String getManufacturer() {
         return null;
