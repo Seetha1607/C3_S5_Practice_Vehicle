a11e66fc8e80a5d1d7f622136cc281ddddffdd69
Seetha <seethalakshmi1607@gmail.com>
2022-08-30 23:32:09 +0530

    created an object of the class car

--- src/main/java/com/vehicles/VehicleService.java
+++ src/main/java/com/vehicles/VehicleService.java
@@ -9,12 +9,16 @@ package com.vehicles;
 
 public class VehicleService {
     public static void main(String[] args) {
+        // Creating an object of the class Bike.
         Bike myBike = new Bike("Honda", "CBR", "SportsBike");
         // Printing the speed of the bike.
         System.out.println("Bike type is SportsBike its speed is " + myBike.maxSpeed("sportsBike") + "km/h");
         // Printing the manufacturer of the bike.
         System.out.println(myBike.getManufacturer());
+        System.out.println();
 
+        // Creating an object of the class Car.
+        Car myCar = new Car("Honda", "City", "Sedan");
 
     }
 }
