6894514e28701cc6ccb5817ba0170e01789d502a
Seetha <seethalakshmi1607@gmail.com>
2022-08-30 13:00:29 +0530

    created abstract method of getManufacturer() method in VehicleManufacturer class

--- src/main/java/com/vehicles/VehicleManufacturer.java
+++ src/main/java/com/vehicles/VehicleManufacturer.java
@@ -35,4 +35,6 @@ public abstract class VehicleManufacturer {
     public void setVehicleType(String vehicleType) {
         this.vehicleType = vehicleType;
     }
+
+    public abstract String getManufacturer();
 }
