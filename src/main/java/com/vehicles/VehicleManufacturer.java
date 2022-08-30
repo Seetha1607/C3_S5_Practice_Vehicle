7f5802e5a168539a9861065ddde7345d4540f35e
Seetha <seethalakshmi1607@gmail.com>
2022-08-30 12:54:46 +0530

    created abstract class VehicleManufacturer.java

---
+++ src/main/java/com/vehicles/VehicleManufacturer.java
@@ -0,0 +1,11 @@
+/*
+ *Author Name : Seetha Lakshmi.G.M
+ *Date : 30-08-2022
+ *Created With : IntelliJ IDEA Community Edition
+ */
+
+
+package com.vehicles;
+
+public abstract class VehicleManufacturer {
+}
