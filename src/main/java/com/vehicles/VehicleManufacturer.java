e213e46715e245d889fd5dda3ef4d70d26e76bf0
Seetha <seethalakshmi1607@gmail.com>
2022-08-30 12:57:23 +0530

    created the adder and setter methods of private fields

--- src/main/java/com/vehicles/VehicleManufacturer.java
+++ src/main/java/com/vehicles/VehicleManufacturer.java
@@ -11,4 +11,28 @@ public abstract class VehicleManufacturer {
     private String vehicleName;
     private String vehicleModelName;
     private String vehicleType;
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    public String getVehicleModelName() {
+        return vehicleModelName;
+    }
+
+    public void setVehicleModelName(String vehicleModelName) {
+        this.vehicleModelName = vehicleModelName;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
 }
