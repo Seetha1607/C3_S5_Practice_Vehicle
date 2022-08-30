105ec9087b3de78aa5784fe26dd6bbd6b0cc2e33
Seetha <seethalakshmi1607@gmail.com>
2022-08-30 23:18:13 +0530

    created the fields of the bike class

--- src/main/java/com/vehicles/Bike.java
+++ src/main/java/com/vehicles/Bike.java
@@ -1,45 +1,15 @@
-/*
- *Author Name : Seetha Lakshmi.G.M
- *Date : 30-08-2022
- *Created With : IntelliJ IDEA Community Edition
- */
-
-
 package com.vehicles;
 
 public class Bike extends VehicleManufacturer implements Vehicle {
     private int maxSpeed;
-
-    public Bike(int maxSpeed) {
-        this.maxSpeed = maxSpeed;
-    }
-
-    public Bike(String vehicleName, String vehicleModelName, String vehicleType, int maxSpeed) {
-        super(vehicleName, vehicleModelName, vehicleType);
-        this.maxSpeed = maxSpeed;
-    }
-
-    public int getMaxSpeed() {
-        return maxSpeed;
-    }
-
-    public void setMaxSpeed(int maxSpeed) {
-        this.maxSpeed = maxSpeed;
-    }
-
-    @Override
-    public int maxSpeed(String vehicleType) {
-        if (vehicleType.equals("sportsBike")) {
-            return 300;
-        } else if (vehicleType.equals("cruiser")) {
-            return 170;
-        }
-        return getMaxSpeed();
-    }
 
     @Override
     public String getManufacturer() {
         return null;
     }
 
+    @Override
+    public int maxSpeed(String vehicleType) {
+        return 0;
+    }
 }
