ce7d244e1113dc134042210a366f59c7deab6658
Seetha <seethalakshmi1607@gmail.com>
2022-08-30 23:28:12 +0530

    created a parametrized constructor of VehicleManufacturer class in car class

--- src/main/java/com/vehicles/Car.java
+++ src/main/java/com/vehicles/Car.java
@@ -4,6 +4,10 @@ public class Car extends VehicleManufacturer implements Vehicle {
     // Declaring a variable.
     private int maxSpeed;
 
+    public Car(String vehicleName, String vehicleModelName, String vehicleType) {
+        super(vehicleName, vehicleModelName, vehicleType);
+    }
+
     @Override
     // A method that returns an integer.
     public int maxSpeed(String vehicleType) {
